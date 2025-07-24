94fd144ec1b1cee7d56ab00663d93c754a0d9489
darius <darius.roberts@gmail.com>
2025-07-24 14:51:10 -0700

    fix test syntax

--- test/cluster_message.test.js
+++ test/cluster_message.test.js
@@ -83,47 +83,47 @@ describe('Msg_m', () => {
     });
 });
 //Message t
-    describe('Msg_t', () => {
-        const raw_ints_t = [51, 51, 51, 51, 51, 51, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 30, 100, 54, 61, 127, 128, 0, 114, 65, 177, 4, 61, 65, 177, 132, 60, 147, 8, 85, 65, 16, 52, 49, 90, 68, 95, 168, 191, 67, 52, 187, 80, 65, 0, 0, 228, 65];
-        const raw_dv = new DataView(new Uint8Array(raw_ints).buffer);
-        const raw_dv_t = new DataView(new Uint8Array(raw_ints_t).buffer);
-    
-        it('should show raw for Msg_t', () => {
-            let m = Msg_t.fromDataView(raw_dv_t);
-            let r = m.raw();
-            expect(r[0][0]).to.equal(raw_ints_t.slice(0, 6)[0]);
-            expect(r[1][0]).to.equal(raw_ints_t.slice(6, 6 + 6)[0]);
-            expect(r[2][0]).to.equal(raw_ints_t.slice(12, 22)[0]);
-            expect(r[3][0]).to.equal(raw_ints_t.slice(22, 23)[0]);
-        });
-    
-        it('should show rawBodyDataView for Msg_t', () => {
-            let t = Msg_t.fromDataView(raw_dv_t);
-            expect(t.rawBodyDataView()).toEqual(new DataView(raw_dv_t.buffer, 24));
-        });
-        
-    
-        it('ClusterMessage should have a datetime for Msg_t', () => {
-            let t = ClusterMessage.fromDataView(raw_dv_t);
-            expect(t.getTime().getDate()).to.equal(13);
-            expect(t.timestamp.seconds).to.equal(204388n);
-            expect(t.timestamp.milliseconds).to.equal(910000000);
-        });
-    
-        it('should respond with name and token for Msg_t', () => {
-            let m = Msg_t.fromDataView(raw_dv_t);
-            expect('name: ' + m.getClassName()).to.equal('name: Msg_t');
-            expect('token: ' + m.getClassToken()).to.equal('token: t');
-        });
-    
-        it('should filter if an t-msg', () => {
-            expect(Msg_t.filter()(raw_dv_t)).to.be.true;
-        });
-    
-        it('should not filter if not an t-msg', () => {
-            let non_t_dv = new DataView(new ArrayBuffer(53));
-            expect(Msg_t.filter()(non_t_dv)).to.be.false;
-        });
+describe('Msg_t', () => {
+    const raw_ints_t = [51, 51, 51, 51, 51, 51, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 30, 100, 54, 61, 127, 128, 0, 114, 65, 177, 4, 61, 65, 177, 132, 60, 147, 8, 85, 65, 16, 52, 49, 90, 68, 95, 168, 191, 67, 52, 187, 80, 65, 0, 0, 228, 65];
+    const raw_dv = new DataView(new Uint8Array(raw_ints).buffer);
+    const raw_dv_t = new DataView(new Uint8Array(raw_ints_t).buffer);
+
+    it('should show raw for Msg_t', () => {
+        let m = Msg_t.fromDataView(raw_dv_t);
+        let r = m.raw();
+        expect(r[0][0]).to.equal(raw_ints_t.slice(0, 6)[0]);
+        expect(r[1][0]).to.equal(raw_ints_t.slice(6, 6 + 6)[0]);
+        expect(r[2][0]).to.equal(raw_ints_t.slice(12, 22)[0]);
+        expect(r[3][0]).to.equal(raw_ints_t.slice(22, 23)[0]);
+    });
+
+    it('should show rawBodyDataView for Msg_t', () => {
+        let t = Msg_t.fromDataView(raw_dv_t);
+        expect(t.rawBodyDataView()).to.deep.equal(new DataView(raw_dv_t.buffer, 24));
+    });
+
+
+    it('ClusterMessage should have a datetime for Msg_t', () => {
+        let t = ClusterMessage.fromDataView(raw_dv_t);
+        expect(t.getTime().getDate()).to.equal(13);
+        expect(t.timestamp.seconds).to.equal(204388n);
+        expect(t.timestamp.milliseconds).to.equal(910000000);
+    });
+
+    it('should respond with name and token for Msg_t', () => {
+        let m = Msg_t.fromDataView(raw_dv_t);
+        expect('name: ' + m.getClassName()).to.equal('name: Msg_t');
+        expect('token: ' + m.getClassToken()).to.equal('token: t');
+    });
+
+    it('should filter if an t-msg', () => {
+        expect(Msg_t.filter()(raw_dv_t)).to.be.true;
+    });
+
+    it('should not filter if not an t-msg', () => {
+        let non_t_dv = new DataView(new ArrayBuffer(53));
+        expect(Msg_t.filter()(non_t_dv)).to.be.false;
+    });
 
 });
 
